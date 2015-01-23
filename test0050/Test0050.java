ee1a3f347fdb14f4878cc1294b9e5b16c296aa01
AlejandroFernandezSanchez <alexsolo007@hotmail.com>
2015-01-23 13:42:02 +0100

    Añadadida raiz cuadrada

--- test0050/Test0050.java
+++ test0050/Test0050.java
@@ -44,16 +44,39 @@ public class Test0050
     public int potencia(int raiz, int exp)
     {
 
-        int result = raiz;
+        int result = 1;
         int expo = exp;
-        expo = exp-1;
+        
         while (expo >= 1)
-        {
+        {            
             result = (multip(result, raiz));
             expo= expo -1;
-        }
-           
+        }              
+              
         return result;
+    }
+    
+    public int raizCuadrada(int raiz)
+    {
+        int cont=32;        
+        boolean found = false;
+        
+        while (cont>0 && found==false)
+        {
+            cont= cont-1;
+            if ((potencia(cont, 2)==raiz))
+            {
+                found=true;
+            }
+            
+        }
+        
+        if(cont==0 || cont==32)
+            {
+                cont = -1;
+            }
+            
+        return cont;
     }
     
 
